c3ed89d65363062ec092ec11a12137e9c05d4712
Al Cheong <22002891+meeps123@users.noreply.github.com>
2019-03-25 00:37:06 +0800

    edits

--- main.js
+++ main.js
@@ -1,63 +1,6 @@
 let replicateNum = prompt('How many replicates of counting?');
 let dilutionFactor = prompt('Dilution factor?');
 
-function init () {
-	let master = document.getElementById('master');
-	initHeader(master);
-	initBody(master);
-	initFooter(master);
-}
-
-function initHeader(elem) {
-	let header = `<div class='header flex-container'><span>cyto-helper</span></div>`;
-	elem.innerHTML += header;
-}
-
-function initBody(elem) {
-	for (t = 0; t < replicateNum; t++) {
-		let div = document.createElement('div');
-		div.classList.add('table');
-		div.classList.add('flex-container');
-		let table = document.createElement('table');
-		for (r = 0; r < 4; r++) {
-			let row = table.insertRow(-1);
-			for (c = 0; c < 2; c++) {
-				let cell = row.insertCell(-1);
-				let input = document.createElement('input');
-				input.type = 'number';
-				input.id = `t${t}r${r}c${c}`;
-				if (r % 2 == 0){
-					input.classList.add('live');
-					input.placeholder = 'Live';
-				} else { 
-					input.classList.add('dead');
-					input.placeholder = 'Dead';
-				}
-				cell.appendChild(input);
-			}
-		}
-		div.appendChild(table);
-		elem.appendChild(div);
-	}
-}
-
-function initFooter(elem) {
-	let footer = 
-	`<div class="result flex-container">
-		<div>Average % viable cells:<br><span id='pOutput'>NIL</span></div>
-		<div>Average viable cell density:<br><span id='dOutput'>NIL</span></div>
-	</div>`;
-	elem.innerHTML += footer;
-}
-
-function detectKeyDown(e) {
-	let x = e.which | e.keyCode;
-	if (x == 9) {
-		// Tab is being pressed
-		compute();
-	}
-}
-
 function compute() {
 	let totalData = [];
 	let filledReplicates = replicateNum;
@@ -102,6 +45,65 @@ function compute() {
 	document.getElementById('dOutput').innerHTML = formattedAvgViableCellDensity;
 	console.log(avgViableCellPercentage);
 }
+
+function init () {
+	let master = document.getElementById('master');
+	initHeader(master);
+	initBody(master);
+	initFooter(master);
+}
+
+function initHeader(elem) {
+	let header = `<div class='header flex-container'><span>cyto-helper</span></div>`;
+	elem.innerHTML += header;
+}
+
+function initBody(elem) {
+	for (t = 0; t < replicateNum; t++) {
+		let div = document.createElement('div');
+		div.classList.add('table');
+		div.classList.add('flex-container');
+		let table = document.createElement('table');
+		for (r = 0; r < 4; r++) {
+			let row = table.insertRow(-1);
+			for (c = 0; c < 2; c++) {
+				let cell = row.insertCell(-1);
+				let input = document.createElement('input');
+				input.type = 'number';
+				input.id = `t${t}r${r}c${c}`;
+				if (r % 2 == 0){
+					input.classList.add('live');
+					input.placeholder = 'Live';
+				} else { 
+					input.classList.add('dead');
+					input.placeholder = 'Dead';
+				}
+				input.addEventListener('focus', compute);
+				cell.appendChild(input);
+			}
+		}
+		div.appendChild(table);
+		elem.appendChild(div);
+	}
+}
+
+function initFooter(elem) {
+	let footer = 
+	`<div class="result flex-container">
+		<div>Average % viable cells:<br><span id='pOutput'>NIL</span></div>
+		<div>Average viable cell density:<br><span id='dOutput'>NIL</span></div>
+	</div>`;
+	elem.innerHTML += footer;
+}
+
+function detectKeyDown(e) {
+	let x = e.which | e.keyCode;
+	if (x == 9) {
+		// Tab is being pressed
+		compute();
+	}
+}
+
 
 function sumArray(arr) {
 	if (arr.length == 0) return 0;
